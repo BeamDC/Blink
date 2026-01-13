a102ca97ed72a8cbedf5fcc7900534c7815c30cf
Bin_Laptop <ethan.ramdeen64@gmail.com>
2026-01-13 10:24:06 -0500

    added test_data.txt to allow for writing test code easier. im planning on properly benchmarking this

--- src/main.zig
+++ src/main.zig
@@ -2,18 +2,28 @@ const std = @import("std");
 const lexer = @import("tokenization/lexer.zig").lexer;
 
 pub fn main() !void {
+    // var gpa: std.heap.DebugAllocator(.{}) = .init;
     var gpa: std.heap.DebugAllocator(.{}) = .init;
+    defer _ = gpa.deinit();
     const alloc = gpa.allocator();
 
-    var lex = lexer.init(
-        \\fn main()  {
-        \\  const pi = 3.14159265;
-        \\}
-    );
+    // read the test data
+    const test_path = "test_data.txt";
+    const file = try std.fs.cwd().openFile(test_path, .{});
+    defer file.close();
+
+    const stat = try file.stat();
+    const file_size: usize = @intCast(stat.size);
+
+    const content: []const u8 = try file.readToEndAlloc(alloc, file_size);
+    defer alloc.free(content);
+
+    var lex = lexer.init(content);
     // std.debug.print("Lexer:\n{any}\n", .{lex});
 
     var timer = try std.time.Timer.start();
     const tokens = try lex.tokenize(alloc);
+    defer alloc.free(tokens);
     const elapsed: f64 = @floatFromInt(timer.read());
 
     for (tokens) |token| {
